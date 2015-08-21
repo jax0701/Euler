bb5e12cdd9655d9005e9069649d31c9cecc1d724
Jax <jargalsaikhan.n@gmail.com>
2015-08-21 00:47:04 -0400

    Adding a solution of problem3

--- problem1.c
+++ problem1.c
@@ -1,18 +1,23 @@
-//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//If we list all the natural numbers below 10 that are 
+//multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 //Find the sum of all the multiples of 3 or 5 below 1000.
 
-
 #include <stdio.h>
+#include <time.h>
 
 void main(void)
 {
-     int max_count=200;
-     int index, result;
+     int max_count=1000;
+     int index, result=0;
+     clock_t start_time, end_time;
 
+     start_time = clock();
      for(index=0; index < max_count; index++){
           if (index % 3 == 0 || index % 5 == 0){
 	       result +=index;
 	  }
      }
-     printf("result = %d\n",result);
+     end_time = clock();
+     
+     printf("result = %d time=%f\n",result, (double)(end_time-start_time));
 }
